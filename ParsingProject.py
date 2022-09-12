feb396c5423c9c27a6d138e71ff38f7decc276e5
konstantin <cardinalreshalie95@gmail.com>
2022-09-12 14:19:08 +0300

    Parsi

--- ParsingProject.py
+++ ParsingProject.py
@@ -1,41 +1,29 @@
 
-import requests                                  #добавляем необходимые библиотеки, это реквест для отправки HTTP запросов на разные сайты.
-from bs4 import BeautifulSoup                    #Потом BeautifulSoup из bs4 (библиотека Python для извлечения данных из файлов HTML и XML)
-from time import sleep                           #библиотека для управлением временем, а в этом случае создание задержки перед запросами
+from turtle import ScrolledCanvas
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
 
-headers = {"User-Agent":                         #Создаем словарь с другим именем, что бы сайты не блокировали твои запросы, так как на сайте будет видно что ты БОТ!
-            "Mozilla/5.0 (Windows NT 6.1; en-US; rv:1.9.1.5)Gecko/20091102 Firefox/3.5.5"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; en-US; rv:1.9.1.5)Gecko/20091102 Firefox"}
 
+def get_url():
+    for count in range(1, 8):
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
+        data = soup.find_all("div", class_="col-lg-4 col-md-6 mb-4")
+        for i in data:
+            card_url ="https://scrapingclub.com" + i.find("a").get("href")
+            yield card_url
 
-
-for count in range(1, 8):                        # создаем цикл рендж что бы данная команда повторялась с 1 по 8 страницу! со строкой слип(3) что значит задержка 3 секунды
-    sleep(3)
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={count}" # урл сайт с которого будут взяты данные
-    
-
-    response = requests.get(url, headers=headers)                       # это команда requests https://otus.ru/nest/post/1516/
-                                                                        # если коротко то http серия идет Запрос-Ответ
-                                                                        # Самый популярный запрос — GET. Он указывает, что осуществляется попытка извлечь -
-                                                                        # данные из какого-нибудь ресурса. Для выполнения этого запроса используют команду requests.get().
-                                                                        # В вышеописанном примере мы использовали get() для захвата определённого значения, являющегося частью объекта Response
-                                                                        # с последующим помещением этого значения в переменную response.
-                                                                        # Теперь мы получили возможность использовать переменную response для изучения данных, полученных в результате отправки запроса GET.
-            
-    soup = BeautifulSoup(response.text, "lxml")                         #придаем переменной соуп значение и + это lxml это команда для правильного получения текста, так же можно "html.parser"
-    data = soup.find_all("div", class_="col-lg-4 col-md-6 mb-4")        #Придаем переменной дата что бы соуп находила див с указанным классом, это наш первый блок
-                                                                        #Далее, делаем цикл фор для I из data, ишем имя, ищем цену, и картинку. в конце вывод.
-    for i in data:
-
-        name = i.find("h4", class_="card-title").text.replace("\n", "")
-        price = i.find("h5").text
-        url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
-
-        print(name + "\n" + price + "\n" + url_img + "\n\n")
-
-
-
-
-
-
-
-
+def array():
+    for card_url in get_url():
+        response = requests.get(card_url, headers=headers)
+        sleep(3)
+        soup = BeautifulSoup(response.text, "lxml")
+        data = soup.find("div", class_="card mt-4 my-4")
+        name = data.find("h3", class_="card-title").text
+        price = data.find("h4").text
+        text = data.find("p", class_="card-text").text
+        url_img = "card-img-top img-fluid" + data.find("img", class_="card-img-top img-fluid").get("src")
+        yield name, price, text, url_img
